01c5316dbc3ac28fd72aac6ce50a6ba858250d28
Pascal Le Merrer <pascal.lemerrer@orange.com>
2019-02-23 00:22:01 +0100

    Extracting  parsers to a dedicated module

--- src/Grid.elm
+++ src/Grid.elm
@@ -7,8 +7,8 @@ import Html.Styled.Attributes exposing (css, fromUnstyled, style, type_)
 import Html.Styled.Events exposing (onClick, onInput)
 import InfiniteList as IL
 import List.Extra
-import Parser exposing ((|.), (|=), Parser, chompWhile, getChompedString, keyword, oneOf, spaces, succeed, symbol)
-
+import Grid.Parsers exposing (..)
+import Parser exposing (Parser)
 
 type alias Item a =
     { a
@@ -601,115 +601,3 @@ cellStyles properties =
         , width (px <| toFloat (properties.width - cumulatedBorderWidth))
         ]
     ]
-
-
-stringParser : Parser String
-stringParser =
-    getChompedString <| chompWhile Char.isAlphaNum
-
-stringEqualityParser : Parser String
-stringEqualityParser =
-        succeed identity
-            |. spaces
-            |. symbol "="
-            |. spaces
-            |= stringParser
-
-
-lessThanStringParser : Parser String
-lessThanStringParser =
-        succeed identity
-            |. spaces
-            |. symbol "<"
-            |. spaces
-            |= stringParser
-
-greaterThanStringParser : Parser String
-greaterThanStringParser =
-        succeed identity
-            |. spaces
-            |. symbol ">"
-            |. spaces
-            |= stringParser
-
-intEqualityParser : Parser Int
-intEqualityParser =
-        succeed identity
-            |. spaces
-            |. symbol "="
-            |. spaces
-            |= Parser.int
-
-lessThanIntParser : Parser Int
-lessThanIntParser =
-        succeed identity
-            |. spaces
-            |. symbol "<"
-            |. spaces
-            |= Parser.int
-
-greaterThanIntParser : Parser Int
-greaterThanIntParser =
-        succeed identity
-            |. spaces
-            |. symbol ">"
-            |. spaces
-            |= Parser.int
-
-floatEqualityParser : Parser Float
-floatEqualityParser =
-        succeed identity
-            |. spaces
-            |. symbol "="
-            |. spaces
-            |= Parser.float
-
-lessThanFloatParser : Parser Float
-lessThanFloatParser =
-        succeed identity
-            |. spaces
-            |. symbol "<"
-            |. spaces
-            |= Parser.float
-
-greaterThanFloatParser : Parser Float
-greaterThanFloatParser =
-        succeed identity
-            |. spaces
-            |. symbol ">"
-            |. spaces
-            |= Parser.float
-
-boolParser : Parser Bool
-boolParser =
-    oneOf
-    [ succeed True
-        |. keyword "true"
-    , succeed False
-        |. keyword "false"
-    ]
-
-boolEqualityParser : Parser Bool
-boolEqualityParser =
-        succeed identity
-            |. spaces
-            |. symbol "="
-            |. spaces
-            |= boolParser
-
-
-lessThanBoolParser : Parser Bool
-lessThanBoolParser =
-        succeed identity
-            |. spaces
-            |. symbol "<"
-            |. spaces
-            |= boolParser
-
-greaterThanBoolParser : Parser Bool
-greaterThanBoolParser =
-        succeed identity
-            |. spaces
-            |. symbol ">"
-            |. spaces
-            |= boolParser
---
+++ src/Grid/Parsers.elm
@@ -0,0 +1,114 @@
+module Grid.Parsers exposing (..)
+
+import Parser exposing ((|.), (|=), Parser, chompWhile, getChompedString, keyword, oneOf, spaces, succeed, symbol)
+
+stringParser : Parser String
+stringParser =
+    getChompedString <| chompWhile Char.isAlphaNum
+
+stringEqualityParser : Parser String
+stringEqualityParser =
+        succeed identity
+            |. spaces
+            |. symbol "="
+            |. spaces
+            |= stringParser
+
+
+lessThanStringParser : Parser String
+lessThanStringParser =
+        succeed identity
+            |. spaces
+            |. symbol "<"
+            |. spaces
+            |= stringParser
+
+greaterThanStringParser : Parser String
+greaterThanStringParser =
+        succeed identity
+            |. spaces
+            |. symbol ">"
+            |. spaces
+            |= stringParser
+
+intEqualityParser : Parser Int
+intEqualityParser =
+        succeed identity
+            |. spaces
+            |. symbol "="
+            |. spaces
+            |= Parser.int
+
+lessThanIntParser : Parser Int
+lessThanIntParser =
+        succeed identity
+            |. spaces
+            |. symbol "<"
+            |. spaces
+            |= Parser.int
+
+greaterThanIntParser : Parser Int
+greaterThanIntParser =
+        succeed identity
+            |. spaces
+            |. symbol ">"
+            |. spaces
+            |= Parser.int
+
+floatEqualityParser : Parser Float
+floatEqualityParser =
+        succeed identity
+            |. spaces
+            |. symbol "="
+            |. spaces
+            |= Parser.float
+
+lessThanFloatParser : Parser Float
+lessThanFloatParser =
+        succeed identity
+            |. spaces
+            |. symbol "<"
+            |. spaces
+            |= Parser.float
+
+greaterThanFloatParser : Parser Float
+greaterThanFloatParser =
+        succeed identity
+            |. spaces
+            |. symbol ">"
+            |. spaces
+            |= Parser.float
+
+boolParser : Parser Bool
+boolParser =
+    oneOf
+    [ succeed True
+        |. keyword "true"
+    , succeed False
+        |. keyword "false"
+    ]
+
+boolEqualityParser : Parser Bool
+boolEqualityParser =
+        succeed identity
+            |. spaces
+            |. symbol "="
+            |. spaces
+            |= boolParser
+
+
+lessThanBoolParser : Parser Bool
+lessThanBoolParser =
+        succeed identity
+            |. spaces
+            |. symbol "<"
+            |. spaces
+            |= boolParser
+
+greaterThanBoolParser : Parser Bool
+greaterThanBoolParser =
+        succeed identity
+            |. spaces
+            |. symbol ">"
+            |. spaces
+            |= boolParser
