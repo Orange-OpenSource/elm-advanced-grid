009e5c5cb8634663798f39d32d01c2ee107ec791
Thierry Courgeon <thierry.courgeon@orange.com>
2020-02-24 09:58:48 +0100

    Modified "comment column" test after dev

--- cypress/integration/elm-grid-filter-spec.js
+++ cypress/integration/elm-grid-filter-spec.js
@@ -19,142 +19,142 @@ describe('elm grid example', function () {
    const valuesGreaterThan95 = ['95.1', '96.3', '97.5', '98.7']
    const valuesLesserThan4 = ['0', '1.2', '2.4', '3.6']
 
-//    it('should have filters', function () {
-//        cy.visit(url)
-//        cy.get('input[data-testid="filter-Id"]')
-//        cy.get('input[data-testid="filter-Name"]')
-//        cy.get('input[data-testid="filter-Progress"]')
-//        cy.get('input[data-testid="filter-Value"]')
-//    })
-//
-//    it('should contain 45 rows of data when none is filtered', function () {
-//        cy.visit(url)
-//        cy.get('div[data-testid="row"]').should('have.length', 45)
-//    })
-//
-//    it('should filter elements containing an exact given value', function () {
-//        cy.visit(url)
-//        cy.get('input[data-testid="filter-City"]').type("=paris")
-//        let rows = cy.get('div[data-testid="row"]')
-//        rows.should('have.length', 2)
-//        cy.get('div[data-testid="City"]').contains("Paris")
-//    })
-//
-//    it('should filter elements containing at least the given string', function () {
-//        cy.visit(url)
-//        cy.get('input[data-testid="filter-City"]').type("ara")
-//        cy.get('div[data-testid="City"]').then (function($citiesList){
-//            expect($citiesList).to.have.length(3)
-//            for (let i=0; i<3; i++){
-//              expect ($citiesList.eq(`${i}`)).to.contain(`${citiesWithARA[`${i}`]}`)
-//            }
-//        })
-//    })
-//
-//    it('should filter elements greater than a given value', function () {
-//        cy.visit(url)
-//        cy.get('input[data-testid="filter-Value"]').type(">95")
-//        cy.get('div[data-testid="Value"]').then (function($valuesList){
-//            expect($valuesList).to.have.length(4)
-//            for (let i=0; i<3; i++){
-//              expect ($valuesList.eq(`${i}`)).to.contain(`${valuesGreaterThan95[`${i}`]}`)
-//            }
-//        })
-//    })
-//
-//    it('should filter elements lesser than a given value', function () {
-//        cy.visit(url)
-//        cy.get('input[data-testid="filter-Value"]').type("<4")
-//        cy.get('div[data-testid="Value"]').then (function($valuesList){
-//            expect($valuesList).to.have.length(4)
-//            for (let i=0; i<3; i++){
-//              expect ($valuesList.eq(`${i}`)).to.contain(`${valuesLesserThan4[`${i}`]}`)
-//            }
-//        })
-//    })
-//
-//    it('should filter elements containing at least the given string when clicking the "set filters" button', function () {
-//        cy.visit(url)
-//        cy.setFilters()
-//
-//        cy.get('div[data-testid="City"]').then (function($citiesList){
-//            expect($citiesList).to.have.length(36)
-//            for (let i=0; i<5; i++){
-//              expect ($citiesList.eq(`${i}`)).to.contain(`${setFilterCityList[`${i}`]}`)
-//            }
-//        })
-//    })
-//
-//    it('should reset filters when clicking on "Reset filters" button', function () {
-//        cy.visit(url)
-//        cy.get('input[data-testid="filter-Id"]').type("<25")
-//        cy.get('input[data-testid="filter-Name"]').type("me1")
-//        cy.get('input[data-testid="filter-Progress"]').type(">15")
-//        cy.get('input[data-testid="filter-Value"]').type("8")
-//        cy.get('input[data-testid="filter-City"]').type("sa")
-//        let rows = cy.get('div[data-testid="row"]')
-//        rows.should('have.length', 1)
-//        cy.get('div[data-testid="City"]').contains("Osaka")
-//
-//        cy.resetFilters()
-//
-//        cy.get('div[data-testid="City"]').then (function($citiesList){
-//            expect($citiesList).to.have.length(45)
-//            for (let i=0; i<4; i++){
-//              expect ($citiesList.eq(`${i}`)).to.contain(`${defaultOrderCities[`${i}`]}`)
-//            }
-//        })
-//    })
-//
-//   it('should filter elements containing exactly the given string', function () {
-//        cy.visit(url)
-//        cy.get('input[data-testid="filter-City"]').type("=ondon")
-//        let rows = cy.get('div[data-testid="row"]')
-//        rows.should('have.length', 0)
-//        cy.get('input[data-testid="filter-City"]').clear()
-//
-//        cy.get('input[data-testid="filter-City"]').type("=london")
-//        rows = cy.get('div[data-testid="row"]')
-//        rows.should('have.length', 2)
-//        cy.get('input[data-testid="filter-City"]').clear()
-//
-//        cy.get('input[data-testid="filter-City"]').type("=London")
-//        rows = cy.get('div[data-testid="row"]')
-//        rows.should('have.length', 2)
-//        cy.get('div[data-testid="City"]').contains("London")
-//    })
-//
-//    it('should filter elements containing the wished item when using the quick filter', function () {
-//        cy.visit(url)
-//        cy.get('div[data-testid="quickFilter-City"]').click()
-//        cy.get('div[id="openedQuickFilter"] > div').eq(11).click()
-//        let rows = cy.get('div[data-testid="row"]')
-//        rows.should('have.length', 1)
-//        cy.get('div[data-testid="row"]').click()
-//
-//        let status = cy.get('div[data-testid="clickedItem"]')
-//        status.contains("Clicked Item = id:31 - name: name31")
-//        cy.resetFilters()
-//        rows = cy.get('div[data-testid="row"]')
-//        rows.should('have.length', 45)
-//    })
-//
-//    it('should filter elements containing empty value when using the quick filter', function () {
-//        cy.visit(url)
-//        cy.get('div[data-testid="quickFilter-City"]').click()
-//        cy.get('div[id="openedQuickFilter"] > div').eq(0).click()
-//        let rows = cy.get('div[data-testid="row"]')
-//        rows.should('have.length', 2)
-//        cy.get('input[data-testid="allItemSelection"]').click()
-//
-//        let selectedItems = cy.get('ul[data-testid="selectedItems"]').children()
-//        selectedItems.should('have.length', 2)
-//        selectedItems.first().contains("id:33 - name: name33")
-//            .next().contains("id:40 - name: name40")
-//        cy.resetFilters()
-//        rows = cy.get('div[data-testid="row"]')
-//        rows.should('have.length', 45)
-//    })
+    it('should have filters', function () {
+        cy.visit(url)
+        cy.get('input[data-testid="filter-Id"]')
+        cy.get('input[data-testid="filter-Name"]')
+        cy.get('input[data-testid="filter-Progress"]')
+        cy.get('input[data-testid="filter-Value"]')
+    })
+
+    it('should contain 45 rows of data when none is filtered', function () {
+        cy.visit(url)
+        cy.get('div[data-testid="row"]').should('have.length', 45)
+    })
+
+    it('should filter elements containing an exact given value', function () {
+        cy.visit(url)
+        cy.get('input[data-testid="filter-City"]').type("=paris")
+        let rows = cy.get('div[data-testid="row"]')
+        rows.should('have.length', 2)
+        cy.get('div[data-testid="City"]').contains("Paris")
+    })
+
+    it('should filter elements containing at least the given string', function () {
+        cy.visit(url)
+        cy.get('input[data-testid="filter-City"]').type("ara")
+        cy.get('div[data-testid="City"]').then (function($citiesList){
+            expect($citiesList).to.have.length(3)
+            for (let i=0; i<3; i++){
+              expect ($citiesList.eq(`${i}`)).to.contain(`${citiesWithARA[`${i}`]}`)
+            }
+        })
+    })
+
+    it('should filter elements greater than a given value', function () {
+        cy.visit(url)
+        cy.get('input[data-testid="filter-Value"]').type(">95")
+        cy.get('div[data-testid="Value"]').then (function($valuesList){
+            expect($valuesList).to.have.length(4)
+            for (let i=0; i<3; i++){
+              expect ($valuesList.eq(`${i}`)).to.contain(`${valuesGreaterThan95[`${i}`]}`)
+            }
+        })
+    })
+
+    it('should filter elements lesser than a given value', function () {
+        cy.visit(url)
+        cy.get('input[data-testid="filter-Value"]').type("<4")
+        cy.get('div[data-testid="Value"]').then (function($valuesList){
+            expect($valuesList).to.have.length(4)
+            for (let i=0; i<3; i++){
+              expect ($valuesList.eq(`${i}`)).to.contain(`${valuesLesserThan4[`${i}`]}`)
+            }
+        })
+    })
+
+    it('should filter elements containing at least the given string when clicking the "set filters" button', function () {
+        cy.visit(url)
+        cy.setFilters()
+
+        cy.get('div[data-testid="City"]').then (function($citiesList){
+            expect($citiesList).to.have.length(36)
+            for (let i=0; i<5; i++){
+              expect ($citiesList.eq(`${i}`)).to.contain(`${setFilterCityList[`${i}`]}`)
+            }
+        })
+    })
+
+    it('should reset filters when clicking on "Reset filters" button', function () {
+        cy.visit(url)
+        cy.get('input[data-testid="filter-Id"]').type("<25")
+        cy.get('input[data-testid="filter-Name"]').type("me1")
+        cy.get('input[data-testid="filter-Progress"]').type(">15")
+        cy.get('input[data-testid="filter-Value"]').type("8")
+        cy.get('input[data-testid="filter-City"]').type("sa")
+        let rows = cy.get('div[data-testid="row"]')
+        rows.should('have.length', 1)
+        cy.get('div[data-testid="City"]').contains("Osaka")
+
+        cy.resetFilters()
+
+        cy.get('div[data-testid="City"]').then (function($citiesList){
+            expect($citiesList).to.have.length(45)
+            for (let i=0; i<4; i++){
+              expect ($citiesList.eq(`${i}`)).to.contain(`${defaultOrderCities[`${i}`]}`)
+            }
+        })
+    })
+
+   it('should filter elements containing exactly the given string', function () {
+        cy.visit(url)
+        cy.get('input[data-testid="filter-City"]').type("=ondon")
+        let rows = cy.get('div[data-testid="row"]')
+        rows.should('have.length', 0)
+        cy.get('input[data-testid="filter-City"]').clear()
+
+        cy.get('input[data-testid="filter-City"]').type("=london")
+        rows = cy.get('div[data-testid="row"]')
+        rows.should('have.length', 2)
+        cy.get('input[data-testid="filter-City"]').clear()
+
+        cy.get('input[data-testid="filter-City"]').type("=London")
+        rows = cy.get('div[data-testid="row"]')
+        rows.should('have.length', 2)
+        cy.get('div[data-testid="City"]').contains("London")
+    })
+
+    it('should filter elements containing the wished item when using the quick filter', function () {
+        cy.visit(url)
+        cy.get('div[data-testid="quickFilter-City"]').click()
+        cy.get('div[id="openedQuickFilter"] > div').eq(11).click()
+        let rows = cy.get('div[data-testid="row"]')
+        rows.should('have.length', 1)
+        cy.get('div[data-testid="row"]').click()
+
+        let status = cy.get('div[data-testid="clickedItem"]')
+        status.contains("Clicked Item = id:31 - name: name31")
+        cy.resetFilters()
+        rows = cy.get('div[data-testid="row"]')
+        rows.should('have.length', 45)
+    })
+
+    it('should filter elements containing empty value when using the quick filter', function () {
+        cy.visit(url)
+        cy.get('div[data-testid="quickFilter-City"]').click()
+        cy.get('div[id="openedQuickFilter"] > div').eq(0).click()
+        let rows = cy.get('div[data-testid="row"]')
+        rows.should('have.length', 2)
+        cy.get('input[data-testid="allItemSelection"]').click()
+
+        let selectedItems = cy.get('ul[data-testid="selectedItems"]').children()
+        selectedItems.should('have.length', 2)
+        selectedItems.first().contains("id:33 - name: name33")
+            .next().contains("id:40 - name: name40")
+        cy.resetFilters()
+        rows = cy.get('div[data-testid="row"]')
+        rows.should('have.length', 45)
+    })
 
     it('should be allowed to edit a label in City column', function () {
         cy.visit(url)
